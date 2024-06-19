2c30fa61dbcb8e667d78956397d2134e6e9f1899
Kris Williams <115474+kriswill@users.noreply.github.com>
2024-06-18 23:29:51 -0700

    pin nixpkgs to nixos-24.05

--- flake.nix
+++ flake.nix
@@ -2,10 +2,12 @@
   description = "Nix Flake for building bootable ISO images.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
     disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, home-manager, flake-utils, disko, ... }: with flake-utils.lib;
